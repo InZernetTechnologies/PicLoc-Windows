d50ee6b10009ad6502e1a9fd48d908bd748541b1
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-01-23 23:11:42 -0500

    Fixed API_URL
    Changed from HTTP to HTTPS, apparently HTTP caused a pNU error.

--- PicLoc/settings.cs
+++ PicLoc/settings.cs
@@ -8,7 +8,7 @@ namespace PicLoc
 {
     class settings
     {
-        public static readonly String API_URL = "http://picloc.inzernettechnologies.com"; // API CALLBACK URL
+        public static readonly String API_URL = "https://picloc.inzernettechnologies.com"; // API CALLBACK URL
         public static readonly String API_version = "1.02";
         public static readonly String API = API_URL + "/" + API_version;
         public static readonly String APP_version = "1.0.1 | APIv" + API_version; // Version used in headers
