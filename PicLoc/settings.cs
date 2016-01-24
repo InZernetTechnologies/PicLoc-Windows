291148bbd01cba1cc2d88f7d6a934540cd1091ca
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-01-23 23:00:45 -0500

    Updated API version and dynamic header
    Added dynamic header so when you change the API version the APP_version
    for the header changes.

--- PicLoc/settings.cs
+++ PicLoc/settings.cs
@@ -8,9 +8,9 @@ namespace PicLoc
 {
     class settings
     {
-        public static readonly String API_URL = "https://picloc.inzernettechnologies.com"; // API CALLBACK URL
-        public static readonly String API_version = "1.01";
+        public static readonly String API_URL = "http://picloc.inzernettechnologies.com"; // API CALLBACK URL
+        public static readonly String API_version = "1.02";
         public static readonly String API = API_URL + "/" + API_version;
-        public static readonly String APP_version = "1.0.1 | APIv1.01"; // Version used in headers
+        public static readonly String APP_version = "1.0.1 | APIv" + API_version; // Version used in headers
     }
 }
