6acd9894aa216ad50ed74d1e20250088c92e0479
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-02-06 22:11:17 -0500

    Updated to APIv1.03
    Updated setting comment for API URL and set to use APIv1.03 instead of
    1.02

--- PicLoc/settings.cs
+++ PicLoc/settings.cs
@@ -8,8 +8,8 @@ namespace PicLoc
 {
     class settings
     {
-        public static readonly String API_URL = "https://picloc.inzernettechnologies.com"; // API CALLBACK URL
-        public static readonly String API_version = "1.02";
+        public static readonly String API_URL = "https://picloc.inzernettechnologies.com"; // API CALLBACK URL [No slash after]
+        public static readonly String API_version = "1.03";
         public static readonly String API = API_URL + "/" + API_version;
         public static readonly String APP_version = "1.0.1 | APIv" + API_version; // Version used in headers
     }
