41a7b33056298d9c233d55a0431b93428cddafd8
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-02-08 23:50:38 -0500

    Added XAML for signup
    Issue #11 started work on
    
    Button click not working

---
+++ PicLoc/signup.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PicLoc
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class signup : Page
+    {
+        public signup()
+        {
+            this.InitializeComponent();
+        }
+
+        private void signup_button_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            Debug.WriteLine("HERE WHAT THE FUCK TAPPED");
+        }
+
+        private void signup_button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("HERE WHAT THE FUCK CLICK");
+        }
+    }
+}
