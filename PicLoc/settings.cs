1d95e816658c417f4cd98ec9c008709cb1007781
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-02-09 20:02:33 -0500

    Added application dynamic version number
    Added 'APP_dyn_version' to automatically get the version

--- PicLoc/settings.cs
+++ PicLoc/settings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel;
 
 namespace PicLoc
 {
@@ -11,6 +12,7 @@ namespace PicLoc
         public static readonly String API_URL = "https://picloc.inzernettechnologies.com"; // API CALLBACK URL [No slash after]
         public static readonly String API_version = "1.03";
         public static readonly String API = API_URL + "/" + API_version;
-        public static readonly String APP_version = "1.0.1 | APIv" + API_version; // Version used in headers
+        private static readonly String APP_dyn_version = Package.Current.Id.Version.Major + "." + Package.Current.Id.Version.Minor + "." + Package.Current.Id.Version.Build + "." + Package.Current.Id.Version.Revision;
+        public static readonly String APP_version = APP_dyn_version + " | APIv" + API_version; // Version used in headers
     }
 }
