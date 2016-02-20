c26039980312272dd831e44b5ce147805dfa6eba
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-02-20 16:28:48 -0500

    Added ability to add friend
    Added friend with new APIv1.10

---
+++ PicLoc/add_friend.xaml.cs
@@ -0,0 +1,148 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Security.Credentials;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PicLoc
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class add_friend : Page
+    {
+        public add_friend()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            // Make the button visible
+            if (Frame.CanGoBack)
+            {
+                currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+            // Unregister event
+            currentView.BackRequested -= backButton;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            base.OnNavigatedFrom(e);
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            // Make the button visible
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            // Register event
+            currentView.BackRequested += backButton;
+        }
+
+        private void backButton(object sender, BackRequestedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                e.Handled = true;
+                Frame.GoBack();
+            }
+        }
+
+        private async void buttonAddFriend_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var vault = new PasswordVault();
+            PasswordCredential cred;
+            String deviceid = null;
+            try
+            {
+                cred = vault.Retrieve("device_id", "device_id");
+                if (cred != null)
+                {
+                    // we have a device id
+                    Debug.WriteLine("Device ID for login: " + cred.Password);
+                    deviceid = cred.Password;
+                }
+            }
+            catch (Exception exx)
+            {
+
+            }
+
+            var values = new Dictionary<string, string>
+            {
+                { "username", main.static_user },
+                { "password", main.static_pass },
+                { "device_id", deviceid.ToString() },
+                { "friend", textboxUsername.Text }
+            };
+
+            var client = new System.Net.Http.HttpClient();
+
+            var content = new FormUrlEncodedContent(values);
+
+            client.DefaultRequestHeaders.Add("device", "WindowsClient/" + settings.APP_version);
+
+            var response = await client.PostAsync(settings.API + "/add_friend/", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Debug.WriteLine("Login response: " + responseString);
+
+            JObject array = null;
+
+            try
+            {
+                array = JObject.Parse(responseString);
+            }
+            catch (Exception ex)
+            {
+                var dlg = new MessageDialog("Unexpected response from server: " + responseString);
+                dlg.Commands.Add(new UICommand("Dismiss", null, "1"));
+                var op = await dlg.ShowAsync();
+                return;
+            }
+
+            Debug.WriteLine(array["status"].ToString());
+
+            if (array["status"].ToString() == "True")
+            {
+
+                var dlg = new MessageDialog("Added " + textboxUsername.Text);
+                dlg.Title = "Successfully added";
+                dlg.Commands.Add(new UICommand("Dismiss", null, "1"));
+                var op = await dlg.ShowAsync();
+
+                Frame.Navigate(typeof(snapscreen));
+            }
+            else
+            {
+                var dlg = new MessageDialog(array["message"].ToString());
+                dlg.Title = "Something went wrong: " + array["code"];
+                dlg.Commands.Add(new UICommand("Dismiss", null, "1"));
+                var op = await dlg.ShowAsync();
+            }
+        }
+    }
+}
