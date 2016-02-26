d1f1d3f57ed005f1db0f826fdbbb6971f259ae82
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-02-25 21:56:01 -0500

    Added add friend
    Added add friend

--- PicLoc/add_friend.xaml.cs
+++ PicLoc/add_friend.xaml.cs
@@ -1,66 +1,22 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Security.Credentials;
 using Windows.UI.Core;
-using Windows.UI.Popups;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace PicLoc
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class add_friend : Page
     {
+        account a = new account();
+        helper h = new helper();
         public add_friend()
         {
             this.InitializeComponent();
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            base.OnNavigatedFrom(e);
             var currentView = SystemNavigationManager.GetForCurrentView();
-            // Make the button visible
-            if (Frame.CanGoBack)
-            {
-                currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
-            }
-            else
-            {
-                currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
-            }
-            // Unregister event
-            currentView.BackRequested -= backButton;
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-            base.OnNavigatedFrom(e);
-            var currentView = SystemNavigationManager.GetForCurrentView();
-            // Make the button visible
             currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
-            // Register event
             currentView.BackRequested += backButton;
         }
-
         private void backButton(object sender, BackRequestedEventArgs e)
         {
             if (Frame.CanGoBack)
@@ -70,78 +26,27 @@ namespace PicLoc
             }
         }
 
-        private async void buttonAddFriend_Tapped(object sender, TappedRoutedEventArgs e)
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
-            var vault = new PasswordVault();
-            PasswordCredential cred;
-            String deviceid = null;
-            try
+            base.OnNavigatedFrom(e);
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.BackRequested -= backButton;
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private async void button_add_friend_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            String loginJSON = await a.addFriend(main.static_user, main.static_pass, progress_bar, textBox_username.Text);
+            JObject jo = JObject.Parse(loginJSON);
+
+            if (jo["status"].ToString() == "True")
             {
-                cred = vault.Retrieve("device_id", "device_id");
-                if (cred != null)
-                {
-                    // we have a device id
-                    Debug.WriteLine("Device ID for login: " + cred.Password);
-                    deviceid = cred.Password;
-                }
-            }
-            catch (Exception exx)
-            {
-
-            }
-
-            var values = new Dictionary<string, string>
-            {
-                { "username", main.static_user },
-                { "password", main.static_pass },
-                { "device_id", deviceid.ToString() },
-                { "friend", textboxUsername.Text }
-            };
-
-            var client = new System.Net.Http.HttpClient();
-
-            var content = new FormUrlEncodedContent(values);
-
-            client.DefaultRequestHeaders.Add("device", "WindowsClient/" + settings.APP_version);
-
-            var response = await client.PostAsync(settings.API + "/add_friend/", content);
-
-            var responseString = await response.Content.ReadAsStringAsync();
-
-            Debug.WriteLine("Login response: " + responseString);
-
-            JObject array = null;
-
-            try
-            {
-                array = JObject.Parse(responseString);
-            }
-            catch (Exception ex)
-            {
-                var dlg = new MessageDialog("Unexpected response from server: " + responseString);
-                dlg.Commands.Add(new UICommand("Dismiss", null, "1"));
-                var op = await dlg.ShowAsync();
-                return;
-            }
-
-            Debug.WriteLine(array["status"].ToString());
-
-            if (array["status"].ToString() == "True")
-            {
-
-                var dlg = new MessageDialog("Added " + textboxUsername.Text);
-                dlg.Title = "Successfully added";
-                dlg.Commands.Add(new UICommand("Dismiss", null, "1"));
-                var op = await dlg.ShowAsync();
-
-                Frame.Navigate(typeof(snapscreen));
+                Frame.Navigate(typeof(snap_screen));
+                h.showSingleButtonDialog("Success", "Successfully added friend [" + textBox_username.Text + "]", "Dismiss");
             }
             else
             {
-                var dlg = new MessageDialog(array["message"].ToString());
-                dlg.Title = "Something went wrong: " + array["code"];
-                dlg.Commands.Add(new UICommand("Dismiss", null, "1"));
-                var op = await dlg.ShowAsync();
+                h.showSingleButtonDialog("Server error [" + jo["code"] + "]", ((jo["message"] != null) ? jo["message"].ToString() : "No server message was provided"), "Dismiss");
             }
         }
     }
