dda11538b934c41504e2f730addfc4bcee172549
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-02-25 21:53:48 -0500

    Added classes
    Added classes: Snap and Friend

---
+++ PicLoc/classes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PicLoc
+{
+    public class classes
+    {
+
+        public class Snap
+        {
+            public int id { get; set; }
+            public string from { get; set; }
+            public string status { get; set; }
+            public BitmapImage ImageSource { get; set; }
+            public int time { get; set; }
+        }
+
+        public class Friend
+        {
+            public string username { get; set; }
+            public string status { get; set; }
+            public string display_name { get; set; }
+        }
+    }
+}
