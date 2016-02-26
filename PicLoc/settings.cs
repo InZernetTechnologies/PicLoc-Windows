54005d5ae3813633905ef3ae7f03615155e76ef1
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-02-25 21:49:31 -0500

    Updated to APIv1.10

--- PicLoc/settings.cs
+++ PicLoc/settings.cs
@@ -9,8 +9,8 @@ namespace PicLoc
 {
     class settings
     {
-        public static readonly String API_URL = "https://picloc.inzernettechnologies.com"; // API CALLBACK URL [No slash after]
-        public static readonly String API_version = "1.03";
+        public static readonly String API_URL = "https://picloc.inzernettechnologies.com/"; // API CALLBACK URL [No slash after]
+        public static readonly String API_version = "1.10";
         public static readonly String API = API_URL + "/" + API_version;
         private static readonly String APP_dyn_version = Package.Current.Id.Version.Major + "." + Package.Current.Id.Version.Minor + "." + Package.Current.Id.Version.Build + "." + Package.Current.Id.Version.Revision;
         public static readonly String APP_version = APP_dyn_version + " | APIv" + API_version; // Version used in headers
